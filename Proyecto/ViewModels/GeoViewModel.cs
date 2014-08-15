db1747087b4cf4e6277f23a69384a81db8df59c0
darioMazzini <dario_rhcp_90@hotmail.com>
2014-08-15 12:06:50 +0900

    Campo Direccion+ Intento de Mapa

---
+++ Proyecto/ViewModels/GeoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.ViewModels
+{
+    public class GeoViewModel
+    {
+        public string Direccion { get; set; }
+    }
+}
